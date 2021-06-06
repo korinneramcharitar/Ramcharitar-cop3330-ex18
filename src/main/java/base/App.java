450697511b32c09378feee2f200004d31a1949c9
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-06 19:01:33 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,44 @@
+package base;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Press C to convert from Fahrenheit to Celsius." + "\n" + "Press F to convert from Celsius to Fahrenheit.");
+        String temp = input.nextLine();
+        if (temp.equals("C")) {
+            System.out.println("Your choice: C");
+            System.out.println("Please enter the Temperature in Fahrenheit:");
+            String temp1 = input.nextLine();
+            int a = Integer.parseInt(temp1);
+            int permtemp1 = ((a - 32) * 5 / 9);
+            System.out.println("The temperature in Celsius is " + permtemp1 + ".");
+        }
+
+        if (temp.equals("c")) {
+            System.out.println("Your choice: C");
+            System.out.println("Please enter the Temperature in Fahrenheit:");
+            String temp1 = input.nextLine();
+            int a = Integer.parseInt(temp1);
+            int permtemp1 = ((a - 32) * 5 / 9);
+            System.out.println("The temperature in Celsius is " + permtemp1 + ".");
+        }
+        if (temp.equals("F")) {
+            System.out.println("Your choice: C");
+            System.out.println("Please enter the Temperature in Fahrenheit:");
+            String temp1 = input.nextLine();
+            int a = Integer.parseInt(temp1);
+            int permtemp1 = ((a - 32) * 5 / 9);
+            System.out.println("The temperature in Celsius is " + permtemp1 + ".");
+        }
+        if (temp.equals("f")) {
+            System.out.println("Your choice: C");
+            System.out.println("Please enter the Temperature in Fahrenheit:");
+            String temp1 = input.nextLine();
+            int a = Integer.parseInt(temp1);
+            int permtemp1 = ((a - 32) * 5 / 9);
+            System.out.println("The temperature in Celsius is " + permtemp1 + ".");
+        }
+        }
+        }
+
